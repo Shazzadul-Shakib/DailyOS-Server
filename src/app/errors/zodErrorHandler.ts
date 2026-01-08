8f717566879b07995c176d62db5242e7f1d1e060
AH-Ratul <ahratul21@gmail.com>
2026-01-08 20:47:12 +0600

    prettier updated

--- src/app/errors/zodErrorHandler.ts
+++ src/app/errors/zodErrorHandler.ts
@@ -2,7 +2,7 @@ import { ZodError } from 'zod';
 import { TError, TGenericErrorResponse } from '../interfaces/error.interface';
 
 export const handleZodError = (err: ZodError): TGenericErrorResponse => {
-  const error: TError = err.issues.map((issue) => {
+  const error: TError = err.issues.map(issue => {
     return {
       path: issue?.path[issue.path.length - 1],
       message: issue?.message,
