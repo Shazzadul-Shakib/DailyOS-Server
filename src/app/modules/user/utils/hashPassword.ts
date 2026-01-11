964fcfd0663341a5fdec8aba37c50f6797761780
AH-Ratul <ahratul21@gmail.com>
2026-01-11 17:19:36 +0600

    feat(auth): implement password hashing comparison helper

--- src/app/modules/user/utils/hashPassword.ts
+++ src/app/modules/user/utils/hashPassword.ts
@@ -1,8 +1,17 @@
 import bcrypt from 'bcryptjs';
-import { config } from '../../../config';
+import { envConfig } from '../../../config';
 
 export async function hashPassword(password: string): Promise<string> {
-  const salt = await bcrypt.genSalt(Number(config.salt));
+  const salt = await bcrypt.genSalt(Number(envConfig.salt));
   const hash = await bcrypt.hash(password, salt);
   return hash;
+}
+
+export async function comparePassword(
+  password: string,
+  isUserPassword: string
+): Promise<boolean> {
+  const compare = await bcrypt.compare(password, isUserPassword);
+
+  return compare;
 }
