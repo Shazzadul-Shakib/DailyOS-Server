1324901747ed5c3df48e853afb469b23cdb23516
AH-Ratul <ahratul21@gmail.com>
2026-01-11 17:08:43 +0600

    feat(config): implement JWT access and refresh token env schema

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -3,8 +3,14 @@ import path from 'path';
 
 dotenv.config({ path: path.join(process.cwd(), '.env') });
 
-export const config = {
+export const envConfig = {
   PORT: process.env.PORT || 5000,
   node_env: process.env.NODE_ENV,
-  salt: process.env.SALT || 7,
+  salt: process.env.SALT,
+  jwt: {
+    jwt_access_secret: process.env.JWT_ACCESS_SECRET,
+    jwt_access_expires: process.env.JWT_ACCESS_EXPIRES_IN,
+    jwt_refresh_secret: process.env.JWT_REFRESH_SECRET,
+    jwt_refresh_expires: process.env.JWT_REFRESH_EXPIRES_IN,
+  },
 };
