19c79168cf2d75d90ab52b89e1655eaee6acf012
AH-Ratul <ahratul21@gmail.com>
2026-01-11 17:14:14 +0600

    feat(user): add controller for retrieving all users

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -3,12 +3,13 @@ import { sendResponse } from '../../utils/sendResponse';
 import status from 'http-status';
 import { userService } from './user.service';
 import { IUser } from './user.interface';
+import { Request, Response } from 'express';
 
 // ----- Register a new user ----- //
 const registerUser = catchAsync(async (req, res) => {
   const user: IUser = req.body;
   const result = await userService.registerUser(user);
-  
+
   sendResponse(res, {
     statusCode: status.CREATED,
     success: true,
@@ -17,6 +18,18 @@ const registerUser = catchAsync(async (req, res) => {
   });
 });
 
+const getAllUsers = catchAsync(async (req: Request, res: Response) => {
+  const result = await userService.getAllUsers();
+
+  sendResponse(res, {
+    statusCode: status.OK,
+    success: true,
+    message: 'Users retrieved successfully',
+    data: result,
+  });
+});
+
 export const userController = {
   registerUser,
+  getAllUsers,
 };
