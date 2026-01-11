95773e8e894847ecd1147ca170f08afd4ec82266
AH-Ratul <ahratul21@gmail.com>
2026-01-11 17:18:22 +0600

    feat(user): implement GET / route for admin to fetch all users

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -3,8 +3,12 @@ import { userController } from './user.controller';
 import { validateRequest } from '../../middleware/validateRequest';
 import { UserValidation } from './user.validation';
 import { Router } from 'express';
+import { checkAuth } from '../../middleware/checkAuth';
+import { Role } from '../../../generated/prisma/enums';
 
 export const userRouter: Router = express.Router();
+
+userRouter.get('/', checkAuth(Role.ADMIN), userController.getAllUsers);
 
 userRouter.post(
   '/register',
