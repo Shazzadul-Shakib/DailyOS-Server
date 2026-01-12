3c4d84e6156e0962b21fe0b93f223010cfd6af1e
AH-Ratul <ahratul21@gmail.com>
2026-01-12 17:03:58 +0600

    remove: auth route

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,16 +1,11 @@
 import { Router } from 'express';
 import { userRouter } from '../modules/user/user.route';
-import { authRouter } from '../modules/auth/auth.route';
 
 export const appRoutes: Router = Router();
 const moduleRoutes: Array<{ path: string; router: Router }> = [
   {
     path: '/user',
     router: userRouter,
-  },
-  {
-    path: '/auth',
-    router: authRouter,
   },
 ];
 
