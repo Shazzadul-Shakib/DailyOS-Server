356231714d04c3a19de41e10e6e1c9c62d51f9ca
AH-Ratul <ahratul21@gmail.com>
2026-01-12 17:08:19 +0600

    feat(auth): refactor login schema and fix type inference

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -20,6 +20,18 @@ const createUserValidationSchema = z.object({
   }),
 });
 
+const credentialsLoginSchema = z.object({
+  body: z.object({
+    email: z.email({ message: 'Invalid email address.' }),
+    password: z
+      .string()
+      .min(6, { message: 'Password must be at least 6 characters long.' }),
+  }),
+});
+
 export const UserValidation = {
   createUserValidationSchema,
+  credentialsLoginSchema,
 };
+
+export type TCredentialsLoginPayload = z.infer<typeof credentialsLoginSchema>['body'];
